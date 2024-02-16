fba170b1d88250f7cd460aec3e8aa4d16d0a7693
marketcalls <rajandran@marketcalls.in>
2024-02-16 23:19:22 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+
+# Slack Bot with Google Gemini
+
+This Slack Bot integrates Google Gemini's AI technology into Slack, offering a smart, responsive assistant that enhances productivity. Follow these steps to set up and deploy your bot.
+
+## Resources
+
+For a detailed step-by-step guide on building this digital assistant, visit [Building a Digital Assistant in Slack with Google Gemini: A Step-by-Step Guide](https://www.marketcalls.in/python/building-a-digital-assistant-in-slack-with-google-gemini-a-step-by-step-guide.html).
+
+## Prerequisites
+
+- Google Gemini API Key
+- Slack App with OAuth & Permissions configured
+- Python environment with necessary libraries
+
+## Setup Instructions
+
+1. **Obtain Google Gemini API Key**: Visit Google AI Studio and generate an API key for the Gemini API.
+2. **Generate Slack App**: Create a new app in Slack and configure basic information, including OAuth scopes required for the bot to function.
+3. **Setup Event Subscriptions**: Subscribe to bot events in your Slack app settings to listen for and respond to messages.
+4. **Install Your App**: Add your app to your workspace with the necessary permissions.
+5. **Development Environment**:
+   - Use Visual Studio Code or your preferred IDE.
+   - Create a Python virtual environment and activate it.
+   - Install necessary Python libraries including Flask, Slack SDK, and Google Gemini libraries.
+6. **Implement Your Slack Bot**: Use Flask for handling Slack events and Google Gemini for generating responses.
+7. **Deploy Your Application**: Choose between local testing with ngrok or deploying to Google Cloud App Engine for production.
+8. **Configure Slack Event Subscriptions**: Update your Slack app's event subscriptions with your deployment's request URL.
+9. **Access Your Slack Bot**: Test your bot via direct message or by mentioning it in a channel.
+
+## Dependencies
+
+List all necessary Python libraries and their installation commands, e.g., `Flask`, `slack_sdk`, `google-generativeai`.
+
+## Deployment
+
+Detailed steps for deploying with ngrok for local testing and Google Cloud App Engine for production, including setting environment variables and configuring app.yaml for GCP.
+
+## Usage
+
+Instructions on how to interact with your bot within Slack, including app mentions and direct messages.
+
+## Contributing
+
+Invite contributions by providing guidelines for submitting pull requests to your project.
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+
---
+++ app.py
@@ -0,0 +1,98 @@
+from flask import Flask, request, jsonify
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+import threading
+import pathlib
+import textwrap
+import os
+from dotenv import load_dotenv
+
+import google.generativeai as genai
+from threading import Thread
+
+processed_ids = set()
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Define Google API Key and Set Gemini Pro Model
+google_api_key = os.getenv('GOOGLE_API_KEY')
+genai.configure(api_key=google_api_key)
+model = genai.GenerativeModel('gemini-pro')
+
+# Initialize a Web Client with the Slack bot token from the environment variables
+slack_token = os.getenv('SLACK_BOT_TOKEN')
+client = WebClient(token=slack_token)
+
+# Get BOT_USER_ID from environment variables
+BOT_USER_ID = os.getenv('BOT_USER_ID')
+app = Flask(__name__)
+
+
+def handle_event_async(data):
+    thread = Thread(target=handle_event, args=(data,), daemon=True)
+    thread.start()
+
+def handle_event(data):
+    event = data["event"]
+    print(f'Received Event Text {event["text"]}')
+    
+    # Check if the event is a message without a subtype (excluding bot messages, etc.)
+   
+
+    if "text" in event and event["type"] == "message" and event.get("subtype") is None:
+        print(f'Received Event Text: {event["text"]}')
+        # Ignore messages from the bot itself
+        if event.get("user") == BOT_USER_ID:
+            return
+
+        # Handle direct message or app mention
+        if event["channel"].startswith('D') or event.get("channel_type") == 'im':
+            # Handle direct message event
+            try:
+                gemini = model.generate_content(event["text"])
+                textout = gemini.text.replace("**", "*")
+                print(textout)
+                response = client.chat_postMessage(
+                    channel=event["channel"],
+                    text=textout,
+                    mrkdwn=True
+                )
+            except SlackApiError as e:
+                print(f"Error posting message: {e.response['error']}")
+
+  
+
+    elif event["type"] == "app_mention" and event.get("client_msg_id") not in processed_ids:
+            try:
+                gemini = model.generate_content(event["text"])
+                textout = gemini.text.replace("**", "*")
+                print(textout)
+                response = client.chat_postMessage(
+                    channel=event["channel"],
+                    text=textout,
+                    mrkdwn=True
+                )
+                processed_ids.add(event.get("client_msg_id"))
+            except SlackApiError as e:
+                print(f"Error posting message: {e.response['error']}")
+
+@app.route('/gemini', methods=['GET']) 
+def helloworld(): 
+    if request.method == 'GET': 
+        gemini = model.generate_content("Hi")
+        return gemini.text
+
+@app.route("/slack/events", methods=["POST"])
+def slack_events():
+    data = request.json
+    if "challenge" in data:
+        return jsonify({"challenge": data["challenge"]})
+    
+    if "event" in data:
+        handle_event_async(data)
+    
+    return "", 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
